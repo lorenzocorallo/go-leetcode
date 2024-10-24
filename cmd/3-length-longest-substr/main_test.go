284c5fc8988d22c8a90ec274acbb464e187eaa30
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2024-10-24 22:35:07 +0200

    fix: run test for 3. in fixed order

--- cmd/3-length-longest-substr/main_test.go
+++ cmd/3-length-longest-substr/main_test.go
@@ -5,25 +5,31 @@ import (
 	"testing"
 )
 
+type input struct {
+	s   string
+	exp int
+}
+
 func Test(t *testing.T) {
-	m := make(map[string]int)
+	inputs := []input{
+		{s: "", exp: 0},
+		{s: " ", exp: 1},
+		{s: "b", exp: 1},
+		{s: "abcabcbb", exp: 3},
+		{s: "bbbbb", exp: 1},
+		{s: "pwwkew", exp: 3},
+		{s: "abcdefg", exp: 7},
+		{s: "abcd fg", exp: 7},
+		{s: "ab-d fg", exp: 7},
+		{s: "ab-dbfg", exp: 5},
+		{s: "tmmzuxt", exp: 5},
+	}
 
-	m[""] = 0
-	m[" "] = 1
-	m["b"] = 1
-	m["abcabcbb"] = 3
-	m["bbbbb"] = 1
-	m["pwwkew"] = 3
-	m["abcdefg"] = 7
-	m["abcd fg"] = 7
-	m["ab-d fg"] = 7
-	m["ab-dbfg"] = 5
-
-	for s, exp := range m {
-		fmt.Printf("\nTEST \"%s\"\n", s)
-		res := lengthOfLongestSubstring(s)
-		if res != exp {
-			t.Fatalf("str: %s, exp: %d, res: %d\n", s, exp, res)
+	for _, in := range inputs {
+		fmt.Printf("\nTEST \"%s\"\n", in.s)
+		res := lengthOfLongestSubstring(in.s)
+		if res != in.exp {
+			t.Fatalf("str: %s, exp: %d, res: %d\n", in.s, in.exp, res)
 		}
 	}
 }
